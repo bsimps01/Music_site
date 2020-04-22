628caaf14ed2371423eaf58ee075570b5a3cd509
Ben Simpson <b.albertsimpson@gmail.com>
2020-04-21 19:39:03 -0700

    updates to working class

--- music/urls.py
+++ music/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('music/<int:song_id>/', views.detail, name='detail')
+    path('', views.home, name='home'),
+    path('music/musician/<int:musician_id>/', views.musician_detail, name='musician_detail'),
+    path('music/album/<int:album_id>/', views.album_detail, name='album_detail'),
+    path('music/song/<int:song_id>/', views.song_detail, name='song_detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -5,13 +5,26 @@ from .models import *
 
 def home(request):
     context = {
-        'song': Song.objects.all()
+        'musicians': Musician.objects.all()
     }
-    return render(request, 'home.html', context)
+    return render(request, 'music/home.html', context)
 
 
-def detail(request, song_id):
+def musician_detail(request, musician_id):
     context = {
-        'song': Song.objects.get(id=music_id)
+        'musician': Musician.objects.get(id=musician_id)
     }
-    return render(request, 'detail.html', context)
+    return render(request, 'music/detail.html', context)
+
+def album_detail(request, album_id):
+    context = {
+        'album': Album.objects.get(id=album_id)
+    }
+    return render(request, 'music/album_detail.html', context)
+
+
+def song_detail(request, song_id):
+    context = {
+        'song': Song.objects.get(id=song_id)
+    }
+    return render(request, 'music/song_detail.html', context)
